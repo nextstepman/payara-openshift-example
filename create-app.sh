c2843031e1ab16d1613f2ae07fc017dbddd11747
Richard Scholz <richard.scholz@senacor.com>
2016-10-05 15:43:47 +0000

    fixed errors in create script

--- create-app.sh
+++ create-app.sh
@@ -1,3 +1,3 @@
-APPNAME=person-repository2
+APPNAME=person-repository
 SOURCE_REPOSITORY_URL=https://github.com/PeterWippermann/person-repository.git
 oc new-app --template payara-maven3 --param APPNAME=$APPNAME,SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL
--- create.sh
+++ create.sh
@@ -1,3 +1,3 @@
-oc new-project pyr-s2i
-oc create -f is.yml 
-oc create -f bc.yml
+oc create -f pyr-s2i-is.yml 
+oc create -f pyr-s2i-bc.yml
+oc create -f payara-maven3-template.yaml
